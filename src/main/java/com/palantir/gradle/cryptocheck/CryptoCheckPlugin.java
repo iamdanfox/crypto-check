e61c2dd342409cb7295823b7cc19c1d6686c533c
Dan Fox <dfox@palantir.com>
2017-08-14 15:16:52 +0100

    TODOs

--- src/main/java/com/palantir/gradle/cryptocheck/CryptoCheckPlugin.java
+++ src/main/java/com/palantir/gradle/cryptocheck/CryptoCheckPlugin.java
@@ -12,5 +12,13 @@ public class CryptoCheckPlugin implements Plugin<Project> {
     @Override
     public void apply(Project project) {
         project.getTasks().create("cryptocheckTask", CryptoCheckTask.class);
+
+        // TODO(dfox): add a description and group to ensure task appears in ./gradlew tasks
+        // TODO(dfox): make task appear as a dependency of ./gradlew test
+        // TODO(dfox): auto skip is an environment variable is set: 'OVERRIDE_KEY_SAFETY_PROTECTIONS'
+
+
+        // TODO(dfox): automatically set up as a dependency of all test tasks (e.g. integTest)
+        // TODO(dfox): extra task to automatically download & insert the jars ?!?!
     }
 }
